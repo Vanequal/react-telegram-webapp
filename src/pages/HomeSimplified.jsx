c92baa43cb59ab3f91fb068c46b4e955d2d96ed5
Vanequal <adgarovzafar21@gmail.com>
2025-04-05 06:31:19 +0500

    Fix styles

--- src/pages/HomeSimplified.jsx
+++ src/pages/HomeSimplified.jsx
@@ -14,36 +14,36 @@ import microscopeIcon from '../assets/img/microscopeSimple.webp';
 import gearIcon from '../assets/img/gearSimple.webp';
 import headIcon from '../assets/img/headSimple.webp';
 
-function Home() {
+function HomeSimplified() {
   return (
-    <div className="home">
+    <div className="home-simplified">
       <Header showShare={true} />
-      <h1 className="home__title">Название раздела</h1>
+      <h1 className="home-simplified__title">Название раздела</h1>
       
-      <div className="home__icons">
-        <img src={backIcon} alt="Back" className="home__icon" />
-        <img src={timeIcon} alt="Time" className="home__icon" />
-        <img src={pautineIcon} alt="Pautine" className="home__icon" />
-        <img src={messengerIcon} alt="Messenger" className="home__icon" />
-        <img src={resumeIcon} alt="Resume" className="home__icon" />
+      <div className="home-simplified__icons">
+        <img src={backIcon} alt="Back" className="home-simplified__icon" />
+        <img src={timeIcon} alt="Time" className="home-simplified__icon" />
+        <img src={pautineIcon} alt="Pautine" className="home-simplified__icon" />
+        <img src={messengerIcon} alt="Messenger" className="home-simplified__icon" />
+        <img src={resumeIcon} alt="Resume" className="home-simplified__icon" />
       </div>
 
-      <div className="home__buttons">
-        <button className="home__button">Обмен опытом</button>
-        <button className="home__button">Описание</button>
-        <button className="home__button">ИКР</button>
-        <button className="home__button">Модули проекта</button>
+      <div className="home-simplified__buttons">
+        <button className="home-simplified__button">Обмен опытом</button>
+        <button className="home-simplified__button">Описание</button>
+        <button className="home-simplified__button">ИКР</button>
+        <button className="home-simplified__button">Модули проекта</button>
       </div>
 
-      <div className="home__footerIcons">
-        <img src={pigIcon} alt="Pig" className="home__icon" />
-        <img src={headIcon} alt="Head" className="home__icon" />
-        <img src={typewriterIcon} alt="Typewriter" className="home__icon" />
-        <img src={gearIcon} alt="Gear" className="home__icon" />
-        <img src={microscopeIcon} alt="Microscope" className="home__icon" />
+      <div className="home-simplified__footerIcons">
+        <img src={pigIcon} alt="Pig" className="home-simplified__icon" />
+        <img src={headIcon} alt="Head" className="home-simplified__icon" />
+        <img src={typewriterIcon} alt="Typewriter" className="home-simplified__icon" />
+        <img src={gearIcon} alt="Gear" className="home-simplified__icon" />
+        <img src={microscopeIcon} alt="Microscope" className="home-simplified__icon" />
       </div>
     </div>
   );
 }
 
-export default Home;
+export default HomeSimplified;
