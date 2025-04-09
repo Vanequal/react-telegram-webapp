c3f5cf6691a31dfd24aee289d7989b714861a910
Vanequal <adgarovzafar21@gmail.com>
2025-04-09 17:55:48 +0500

    ios fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Header from '../components/UI/Header';
 import HomeIcons from '../components/UI/HomeIcons';
 import FooterIcons from '../components/UI/FooterIcons';
 import '../styles/Home.scss';
@@ -15,7 +14,6 @@ function Home() {
   };
 
   useEffect(() => {
-    // Добавляем Telegram скрипт и инициализацию
     const script = document.createElement('script');
     script.src = "https://telegram.org/js/telegram-web-app.js?version=5.7";
     script.async = true;
@@ -51,50 +49,49 @@ function Home() {
     }
   }, []);
 
-
   return (
     <div className="home">
+      <div className="home__scroll">
+        <section className="section-head">
+          <div className="section-head__left">
+            <img src={require('../assets/img/burger.webp')} alt="Меню" className="section-head__icon" />
+          </div>
+          <div className="section-head__center">
+            <input
+              type="text"
+              placeholder="Поиск"
+              className="section-head__search"
+            />
+          </div>
+          <div className="section-head__right">
+            <img src={require('../assets/img/share.webp')} alt="Поделиться" className="section-head__icon" />
+            <img src={require('../assets/img/bell.webp')} alt="Уведомления" className="section-head__icon" />
+          </div>
+        </section>
 
-      <section className="section-head">
-        <div className="section-head__left">
-          <img src={require('../assets/img/burger.webp')} alt="Меню" className="section-head__icon" />
+        <h1 className="home__title">Название раздела</h1>
+
+        <HomeIcons />
+
+        <div className="home__buttons">
+          <button
+            className={`home__button home__button--primary ${animate ? 'animate' : ''}`}
+            onClick={handleClick}>
+            Обмен&nbsp;опытом
+          </button>
+          <button className="home__button home__button--secondary">
+            Описание
+          </button>
+          <button className="home__button home__button--tertiary">
+            Идеальный результат
+          </button>
+          <button className="home__button home__button--quaternary">
+            Модули проекта
+          </button>
         </div>
-        <div className="section-head__center">
-          <input
-            type="text"
-            placeholder="Поиск"
-            className="section-head__search"
-          />
-        </div>
-        <div className="section-head__right">
-          <img src={require('../assets/img/share.webp')} alt="Поделиться" className="section-head__icon" />
-          <img src={require('../assets/img/bell.webp')} alt="Уведомления" className="section-head__icon" />
-        </div>
-      </section>
 
-
-      <h1 className="home__title">Название раздела</h1>
-
-      <HomeIcons />
-
-      <div className="home__buttons">
-        <button
-          className={`home__button home__button--primary ${animate ? 'animate' : ''}`}
-          onClick={handleClick}>
-          Обмен&nbsp;опытом
-        </button>
-        <button className="home__button home__button--secondary">
-          Описание
-        </button>
-        <button className="home__button home__button--tertiary">
-          Идеальный результат
-        </button>
-        <button className="home__button home__button--quaternary">
-          Модули проекта
-        </button>
+        <FooterIcons />
       </div>
-
-      <FooterIcons />
     </div>
   );
 }
