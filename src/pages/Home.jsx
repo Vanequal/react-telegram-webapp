c6c7d909c789ce851d1795bd403e1d1da1c29832
Vanequal <adgarovzafar21@gmail.com>
2025-04-09 15:54:25 +0500

    fix ios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,30 +7,6 @@ import '../styles/Home.scss';
 function Home() {
   const [animate, setAnimate] = useState(false);
 
-  useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const tg = window.Telegram.WebApp;
-      tg.disableVerticalSwipes();
-      tg.setHeaderColor('#1E1E1E');
-      
-      tg.expand();
-      try {
-        tg.requestFullscreen();
-      } catch (e) {
-        console.warn('requestFullscreen не поддерживается:', e);
-      }
-    }
-
-    document.addEventListener('touchmove', function (e) {
-      if (e.scale !== 1) { e.preventDefault(); }
-    }, { passive: false });
-
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-    if (isIOS) {
-      document.body.classList.add('ios');
-    }
-  }, []);
-
   const handleClick = () => {
     setAnimate(true);
     setTimeout(() => {
@@ -45,19 +21,19 @@ function Home() {
       
       <HomeIcons />
       
-      <div className="homebuttons">
+      <div className="home__buttons">
         <button
-          className={`homebutton homebutton--primary ${animate ? 'animate' : ''}`}
+          className={`home__button home__button--primary ${animate ? 'animate' : ''}`}
           onClick={handleClick}>
           Обмен &nbsp; опытом
         </button>
-        <button className="homebutton homebutton--secondary">
+        <button className="home__button home__button--secondary">
           Описание
         </button>
-        <button className="homebutton homebutton--tertiary">
+        <button className="home__button home__button--tertiary">
           Идеальный результат
         </button>
-        <button className="homebutton homebutton--quaternary">
+        <button className="home__button home__button--quaternary">
           Модули проекта
         </button>
       </div>
