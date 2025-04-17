aee4f1b5f33c6f3eeb10f8dcfd66d635b5d7acfc
Vanequal <adgarovzafar21@gmail.com>
2025-04-17 12:04:18 +0500

    fix layout

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,44 +9,47 @@ import sendIcon from '../assets/img/sendIcon.webp';
 import '../styles/AboutPage.scss';
 
 const AboutPage = () => {
-  return (
-    <div className="about-page">
-      <section className="mind-vault-header">
-        <div className="mind-vault-header__left">
-          <span className="mind-vault-header__back-text">Назад</span>
-        </div>
-        <h1 className="mind-vault-header__title">О ресурсе</h1>
-      </section>
+    return (
+        <div className="about-page">
+            <section className="mind-vault-header">
+                <div className="mind-vault-header__left">
+                    <span className="mind-vault-header__back-text">Назад</span>
+                </div>
+                <h1 className="mind-vault-header__title">О ресурсе</h1>
+            </section>
 
-      <div className="idea-card idea-card--no-header">
-        <h2 className="idea-card__heading">Заголовок</h2>
-        <div className="idea-card__text">
-          Разработать информационный ресурс Project of Everything on Wiki — платформу моделирования будущего, объединяющую интерактивные преимущества успешных механизмов самоорганизации
-          <div className="idea-card__gif-wrapper">
-            <img src={ecologyGif} alt="ecology" className="idea-card__gif" />
-          </div>
-          интернет-энциклопедии Wikipedia, элементы сервисов вопросов и ответов Quora, Stack Exchange, Genon и мессенджера Telegram.
-          Ресурс выступает инструментом для генерации достоверной информации, направленной на коллективное моделирование будущего и объединяет функциональные элементы различных платформ и методологий.
-        </div>
-        <div className="idea-card__edit-icon">
-          <img src={pencilIcon} alt="Edit" />
-        </div>
-        <div className="idea-card__audio">
-          <AudioPlayer />
-        </div>
-      </div>
+            <div className="idea-card idea-card--no-header">
+                <h2 className="idea-card__heading">Заголовок</h2>
+                <div className="idea-card__text">
+                    Разработать информационный ресурс Project of Everything on Wiki — платформу моделирования будущего, объединяющую интерактивные преимущества успешных механизмов самоорганизации
+                    <div className="idea-card__gif-wrapper">
+                        <img src={ecologyGif} alt="ecology" className="idea-card__gif" />
+                    </div>
+                    интернет-энциклопедии Wikipedia, элементы сервисов вопросов и ответов Quora, Stack Exchange, Genon и мессенджера Telegram.
+                    Ресурс выступает инструментом для генерации достоверной информации, направленной на коллективное моделирование будущего и объединяет функциональные элементы различных платформ и методологий.
+                </div>
 
-      <div className="about-page__footer">
-        <img src={skrepkaIcon} alt="Attach" className="about-page__footer-icon" />
-        <input
-          type="text"
-          className="about-page__footer-input"
-          placeholder="Комментировать"
-        />
-        <img src={sendIcon} alt="Send" className="about-page__footer-send" />
-      </div>
-    </div>
-  );
+                <div className="idea-card__audio">
+                    <AudioPlayer />
+                </div>
+
+                <div className="idea-card__edit-icon idea-card__edit-icon--bottom">
+                    <img src={pencilIcon} alt="Edit" />
+                </div>
+            </div>
+
+
+            <div className="about-page__footer">
+                <img src={skrepkaIcon} alt="Attach" className="about-page__footer-icon" />
+                <input
+                    type="text"
+                    className="about-page__footer-input"
+                    placeholder="Комментировать"
+                />
+                <img src={sendIcon} alt="Send" className="about-page__footer-send" />
+            </div>
+        </div>
+    );
 };
 
 export default AboutPage;
