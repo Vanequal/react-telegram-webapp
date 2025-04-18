b064dba8f516671e2220b698304bc5e873c1e03b
Vanequal <adgarovzafar21@gmail.com>
2025-04-18 17:49:34 +0500

    me request fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,22 @@
-// src/api/axios.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://6012-109-75-62-2.ngrok-free.app', 
+  baseURL: 'https://6012-109-75-62-2.ngrok-free.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true'
   }
+});
+
+instance.interceptors.request.use((config) => {
+  const tg = window.Telegram?.WebApp;
+  if (tg?.initData) {
+    config.headers['X-Telegram-InitData'] = tg.initData;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default instance;
