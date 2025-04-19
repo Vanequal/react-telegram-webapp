8c7c6afb6b5731a3abdffb803fb12d15bceeb8ec
Vanequal <adgarovzafar21@gmail.com>
2025-04-19 21:34:59 +0500

    useEffect main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './store';
+
+import React, { useEffect } from 'react';
+
 import App from './App.jsx'
 
 if (window.Telegram?.WebApp) {
