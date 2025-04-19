9d35d4574202a96fb04e819faf3fe3956f45ddd0
Vanequal <adgarovzafar21@gmail.com>
2025-04-19 21:36:41 +0500

    main fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux';
-import store from './store';
-
-import React, { useEffect } from 'react';
+import { Provider } from 'react-redux'
+import store from './store'
 
 import App from './App.jsx'
 
 if (window.Telegram?.WebApp) {
-  window.Telegram.WebApp.ready(); 
-  console.log('initData:', window.Telegram.WebApp.initData); 
+  window.Telegram.WebApp.ready()
+  console.log('✅ Telegram SDK инициализирован')
+  console.log('initData:', window.Telegram.WebApp.initData)
+  console.log('version:', window.Telegram.WebApp.version)
 } else {
-  console.warn('Telegram WebApp не найден');
+  console.warn('Telegram WebApp не найден')
 }
-
-useEffect(() => {
-  console.log('Telegram SDK:', window.Telegram?.WebApp);
-  console.log('version:', window.Telegram?.WebApp?.version); // должен быть 6.7+
-}, []);
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
