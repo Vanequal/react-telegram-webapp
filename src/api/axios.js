0876cdd92ba2e71affa378ed4c6ff4b401efcec2
Vanequal <adgarovzafar21@gmail.com>
2025-05-01 00:00:37 +0500

    new url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://d64b-109-75-62-2.ngrok-free.app',
+  baseURL: 'https://7b8a-109-75-62-2.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true'
