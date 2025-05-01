d8e348876cddb78231ce6ff5a5755c82057146bc
Vanequal <adgarovzafar21@gmail.com>
2025-05-01 19:40:57 +0500

    useefect import

--- src/pages/DiscussionPage.jsx
+++ src/pages/DiscussionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
