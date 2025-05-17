7d7765ff482ec680e1140ea6a64310491b79c28f
Vanequal <adgarovzafar21@gmail.com>
2025-05-18 01:55:37 +0500

    new edpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://555a-109-75-62-2.ngrok-free.app/',
+  baseURL: 'https://oleg-forum-site.matthew-0203.ru',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true'
