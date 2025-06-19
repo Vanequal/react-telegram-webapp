3676372ded9968bb8ea66e011e510fbd2e36f4da
Vanequal <adgarovzafar21@gmail.com>
2025-06-19 05:03:53 +0500

    fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://voice-simon-shoe-disappointed.trycloudflare.com/',
+  baseURL: 'https://bestsellers-revenge-tender-plots.trycloudflare.com',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true'
