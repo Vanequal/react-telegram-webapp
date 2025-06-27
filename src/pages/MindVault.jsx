1562d3e0ae453129bfbf878f5f492c59d75a7f33
Vanequal <adgarovzafar21@gmail.com>
2025-06-27 13:28:04 +0500

    fix

--- src/pages/MindVault.jsx
+++ src/pages/MindVault.jsx
@@ -12,11 +12,11 @@ import { getViewedIdeas, markIdeaAsViewed } from '../utils/utils.js';
 
 // Components
 import MindVaultHeader from '../components/UI/MindVaultHeader';
-import IdeaCard from './components/IdeaCard';
-import PostComposer from './components/PostComposer';
-import EmptyState from './components/EmptyState';
-import LoadingState from './components/LoadingState';
-import ErrorState from './components/ErrorState';
+import IdeaCard from '../components/IdeaCard';
+import PostComposer from '../components/PostComposer';
+import EmptyState from '../components/EmptyState';
+import LoadingState from '../components/LoadingState';
+import ErrorState from '../components/ErrorState';
 
 // Styles
 import '../styles/components/mind-vault.scss';
