3831a0cbfe0994e85bae1a7761cc97e67eb53a20
Vanequal <adgarovzafar21@gmail.com>
2025-08-19 04:53:49 +0500

    fix

--- src/pages/PublicationPage.jsx
+++ src/pages/PublicationPage.jsx
@@ -78,7 +78,7 @@ const PublicationPage = () => {
   }, []);
 
   const handleNavigateBack = useCallback(() => {
-    navigate('/publicationpagelist');
+    navigate('/publicationlist');
   }, [navigate]);
 
   const handleNavigateToAbout = useCallback(() => {
